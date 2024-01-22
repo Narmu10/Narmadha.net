6f943b03e31efce1a4684974628c8a9f72a04e69
Narmadha <narmadha_anbalagan@epam.com>
2024-01-22 21:02:09 +0530

    Insert in Practice2

--- Practice2/Practice2/Program.cs
+++ Practice2/Practice2/Program.cs
@@ -10,6 +10,17 @@ namespace Practice2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+            sbyte a = 11;
+            Console.WriteLine("sbyte");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("type of variable = " + a.GetType());
+            Console.WriteLine("size of sbyte = " + sizeof(sbyte));
+            Console.WriteLine("Min value of sbyte = " + sbyte.MinValue);
+            Console.WriteLine("Max value of sbyte = " + sbyte.MaxValue);
+            Console.ReadLine();
+
         }
     }
 }
