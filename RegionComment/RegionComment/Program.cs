ebe3198154f2605b0a99ddc1ca4f8cdd5d04ff47
Narmadha <narmadha_anbalagan@epam.com>
2024-02-02 00:44:09 +0530

    Added Region comment

---
+++ RegionComment/RegionComment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionComment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10, num2 = 30,sum =0,product=0;
+            #region Parameter
+
+             sum = num1 + num2;
+             product = num1 * num2;
+
+            #endregion
+
+            #region Output
+            Console.WriteLine(product);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+
+            #endregion
+
+
+        }
+    }
+}
